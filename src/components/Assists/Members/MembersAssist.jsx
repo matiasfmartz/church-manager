ed7fcff24842678c5799a6cf119989f1f981f269
matiasfmart <martinez_matias@live.com.ar>
2024-05-01 17:40:43 -0300

    ui-assistance

--- src/components/Assists/Members/MembersAssist.jsx
+++ src/components/Assists/Members/MembersAssist.jsx
@@ -8,8 +8,18 @@ const MembersAssist = () => {
       <div className="h-[8%] flex items-center justify-between flex-column md:flex-row flex-wrap space-y-4 md:space-y-0 pb-4 bg-white">
           <BtnTable placeHolderSearch={"Buscar Miembros"}/>
       </div>
-      <div className="overflow-auto h-[92%]">
+      <div className='grid grid-cols-8 h-[92%]'>
+        <div className="col-start-1 col-span-6 overflow-auto">
           <Table arr={[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]}/>
+        </div>
+        <div className="col-start-7 col-span-2 content-center grid justify-items-center">
+          <button id="dropdownActionButton" data-dropdown-toggle="dropdownAction" className="mx-3 inline-flex items-center text-gray-500 hover:text-blue-600 font-medium text-sm px-3 py-2.5 border-b-2 border-white hover:border-blue-600" type="button">
+              Reuniones Generales
+          </button>
+          <button id="dropdownActionButton" data-dropdown-toggle="dropdownAction" className="mx-3 inline-flex items-center text-gray-500 hover:text-blue-600 font-medium text-sm px-3 py-2.5 border-b-2 border-white hover:border-blue-600" type="button">
+              Reuniones Especiales
+          </button>
+        </div>
       </div>
     </>
   )
